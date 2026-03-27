5c14b3849bc166071b522572aa38dd3f35df3654
JosephHumerez <joseph_humerez@hotmail.com>
2026-03-27 14:14:53 -0400

    Se elimino el "//2 crear un plan nuevo"

--- app/backend/controllers/cobros/planController.tsx
+++ app/backend/controllers/cobros/planController.tsx
@@ -16,24 +16,3 @@ export async function getPlanes() {
     throw new Error("No se pudieron cargar los planes de publicación.");
   }
 }
-
-// 2. Crear un plan nuevo
-export async function createPlan(data: {
-  nombre_plan: string;
-  precio_plan: number; // Tu app envía un número normal
-  cant_publicaciones: number;
-}) {
-  try {
-    const nuevoPlan = await prisma.planPublicacion.create({
-      data: {
-        nombre_plan: data.nombre_plan,
-        precio_plan: data.precio_plan.toString(), 
-        cant_publicaciones: data.cant_publicaciones,
-      },
-    });
-    return nuevoPlan;
-  } catch (error) {
-    console.error("Error al crear el plan:", error);
-    throw new Error("No se pudo crear el plan.");
-  }
-}
