bb343952aa35f1ddbd6666aec9a3c55f7d65b092
Anelise Rodriguez <rodriguez.jimenez.anelisemayte@gmail.com>
2026-04-19 19:56:46 -0400

    mis cambios

---
+++ app/auth/DiscordSignIn.tsx
@@ -0,0 +1,13 @@
+import { discordStyles, discordIcon } from "./stylesButton"
+
+export function SignInDiscord() {
+  const handleClick = () => {
+    console.log("")
+  }
+
+  return (
+    <button onClick={handleClick} className={discordStyles}>
+      {discordIcon}
+    </button>
+  )
+}
---
+++ app/auth/FacebookSignIn.tsx
@@ -0,0 +1,13 @@
+import { facebookStyles, facebookIcon } from "./stylesButton"
+
+export function SignInFacebook() {
+  const handleClick = () => {
+    console.log("")
+  }
+
+  return (
+    <button onClick={handleClick} className={facebookStyles}>
+      {facebookIcon}
+    </button>
+  )
+}
